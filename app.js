995f5458c6436ab2187d64f2cbd440c423984a69
ChaseTheCoder <chasefsheaff@gmail.com>
2020-06-23 19:03:24 -0500

    Commit 4 functions

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ let age = 0;
 let hunger = 1;
 let sleep = 1;
 let bordem = 1;
-let image = document.getElementById('image');
+let imageNormal = document.getElementById('image');
 let imageDead = "https://www.tamatalk.com/IB/uploads/profile/photo-188759.gif";
 
-const nameInput = document.getElementById('nameInput');
-const nameButton = document.getElementById('enterName');
+
 const feedButton = document.getElementById('feed');
 const lightsButton = document.getElementById('lights');
 const playButton = document.getElementById('play');
 
-
+function namePet() {
+    const nameInput = document.getElementById('nameInput');
+    const nameButton = document.getElementById('enterName');
+}
 
 class tamaPet {
 	constructor(name, age, sleep, bordem, image) {
@@ -25,13 +27,14 @@ class tamaPet {
 }
 
 function isAlive {
-    if (tamaPet.sleep < 10 && tamaPet.bordem < 10 && tamaPet.bordem < 10) {
+    if (tamaPet.sleep < 10 && tamaPet.boredem < 10 && tamaPet.bordem < 10) {
         return true; 
-    } else if (tamaPet.sleep == 10 || tamaPet.bordem == 10 || tamaPet.bordem == 10) {
+    } else if (tamaPet.sleep == 10 || tamaPet.bordem == 10 || tamaPet.boredem == 10) {
         return false
         console.log
     }
 }
+console.log(isAlive);
 
 
 
